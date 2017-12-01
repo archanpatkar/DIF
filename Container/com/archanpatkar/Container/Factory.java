2e09fbae946a58249dcef1415e28d97bc4d5ccc4
Archan Patkar <archanpatkar@gmail.com>
2017-12-01 18:16:39 +0530

    Updated Factory.java

--- Container/com/archanpatkar/Container/Factory.java
+++ Container/com/archanpatkar/Container/Factory.java
@@ -1,4 +1,4 @@
-package Container;
+package container;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,11 +10,9 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class Factory {
 
     private final String Path;
-
 
     public String getPath() {
         return Path;
@@ -24,13 +22,14 @@ public class Factory {
         this.Path = Path;
     }
 
-    private static class Field{
+    private static class Field {
+
         private String Name;
         private String Medium;
         private String Type;
         private Object Value;
 
-        public Field(){
+        public Field() {
 
         }
 
@@ -38,6 +37,12 @@ public class Factory {
             this.Name = Name;
             this.Medium = Medium;
             this.Type = Type;
+            this.Value = Value;
+        }
+
+        public Field(String Name, String Medium, Object Value) {
+            this.Name = Name;
+            this.Medium = Medium;
             this.Value = Value;
         }
 
@@ -80,71 +85,63 @@ public class Factory {
 
     }
 
-
-    public Object getInstance(String LogicalName) throws ClassNotFoundException{
+    public Object getInstance(String LogicalName) throws ClassNotFoundException {
+        
         try {
-            JsonNode jn = getPJson(LogicalName, Path);
-            Class ObjectClass = Class.forName(jn.get("Class").asText());
+            JsonNode node = getObjectConfig(LogicalName, Path);
+            Class ObjectClass = Class.forName(node.get("Class").asText());
             System.out.println("Class Name : " + ObjectClass);
-            ArrayList <Field> Fields = new ArrayList();
-            for(JsonNode j : jn.get("Fields")){
-                System.out.println("In ForEach");
-                System.out.println(j);
-                Fields.add(new Field(j.get("name").asText(),j.get("medium").asText(),j.get("type").asText(),(JsonNode)j.get("value")));
-
-            }
-            for(Field F : Fields)
-            {
-                if(F.getMedium().equals("cons")){
-                    return consMedium(ObjectClass,Fields);
+            ArrayList<Field> Fields = new ArrayList();
+            for (JsonNode j : node.get("Fields")) {
+                if (j.get("type") != null) {
+                    Fields.add(new Field(j.get("name").asText(), j.get("medium").asText(), j.get("type").asText(), (JsonNode) j.get("value")));
+                } else {
+                    Fields.add(new Field(j.get("name").asText(), j.get("medium").asText(), (JsonNode) j.get("value")));
                 }
             }
-            Object temp = ObjectClass.newInstance();
-            for(Method m : ObjectClass.getMethods())
-            {
-               for(Field F : Fields){
-                   if(m.getName().equals(F.getMedium() + F.getName().toUpperCase())){
-                       JsonNode n = (JsonNode)F.getValue();
-                       if(n.asText().startsWith("contains"))
-                       {
-                           String [] logicallink = n.asText().split("-");
-                           Object compose = this.getInstance(logicallink[1]);
-                           m.invoke(temp,compose.getClass().cast(compose));
-                       }
-                       switch(F.getType())
-                       {
-                           case "int":m.invoke(temp,n.asInt());break;
-                           case "long":m.invoke(temp,n.asLong());break;
-                           case "double":m.invoke(temp,n.asDouble());break;
-                           case "boolean":m.invoke(temp,n.asBoolean());break;
-                       }
-
-                   }
-               }
+            Object object = ObjectClass.newInstance();
+            for (Method method : ObjectClass.getMethods()) {
+                for (Field field : Fields) {
+                    if (method.getName().equals(field.getMedium() + field.getName().toUpperCase())) {
+                        JsonNode n = (JsonNode) field.getValue();
+                        if (n.asText().startsWith("contains")) {
+                            String[] logicallink = n.asText().split("-");
+                            System.out.println(logicallink);
+                            Object compose = this.getInstance(logicallink[1]);
+                            method.invoke(object, compose.getClass().cast(compose));
+                        } else {
+                            switch (field.getType()) {
+                                case "int":
+                                    method.invoke(object, n.asInt());
+                                    break;
+                                case "long":
+                                    method.invoke(object, n.asLong());
+                                    break;
+                                case "double":
+                                    method.invoke(object, n.asDouble());
+                                    break;
+                                case "boolean":
+                                    method.invoke(object, n.asBoolean());
+                                    break;
+                            }
+                        }
+                    }
+                }
             }
-            return temp;
-        } catch (InstantiationException ex) {
-            Logger.getLogger(Factory.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(Factory.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalArgumentException ex) {
-            Logger.getLogger(Factory.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InvocationTargetException ex) {
+            return object;
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
             Logger.getLogger(Factory.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
 
     }
 
-    private Object consMedium(Class ObjectClass,ArrayList <Field> Fields){
-        System.out.println("Constructor Medium Called");
-        return null;
-    }
-
-    private  JsonNode getPJson(String propn,String Path) {
+    private JsonNode getObjectConfig(String propn, String Path) {
         try {
             return new ObjectMapper().readTree(new FileInputStream(Path)).path(propn);
-        } catch (IOException ex) {System.out.println("Exception");}
+        } catch (IOException ex) {
+            System.out.println("Exception");
+        }
         return null;
     }
 }
